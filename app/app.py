95489ff9f82b82137f98c49312b41551e7182886
franlops <franlopbri@gmail.com>
2023-07-05 13:32:07 -0600

    02 Rutas, Templates y Static

--- app/app.py
+++ app/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home/')
 def home():
-    return '<h1>Hello Flask</h1>'
+    return render_template('home.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact/')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/categories/')
+def categories():
+    return render_template('categories.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
