6d4bc6d335b5dcbca2bfb0f205024bebb178839a
franlops <franlopbri@gmail.com>
2023-07-06 14:26:42 -0600

    05 Delete Update Icons

--- app/app.py
+++ app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import (Flask, render_template, request,
+                   redirect, flash, url_for)
 from db.categories import Category
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'My Secret Key'
 
 @app.route('/')
 @app.route('/home/')
@@ -31,9 +33,29 @@ def create_cat():
     if request.method == 'POST':
         category = request.form['category']
         description = request.form['description']
-        print(category)
-        print(description)
+        if not category:
+            flash('Debes ingresar una Categoría')
+        elif not description:
+            flash('Debes ingresar una descripcion')
+        else:    
+            cat = Category(category, description)
+            cat.save()
+            return redirect(url_for('categories'))
     return render_template('create_cat.html')
+
+@app.route('/categories/<int:id>/update/')
+def update_cat(id):
+    #Obtener Cat desde id
+    #Enviar datos a form
+    return "Lo vamos a Actualizar"
+
+@app.route('/categories/<int:id>/delete/')
+def delete_cat(id):
+    #Obtener Cat desde id
+    cat = Category.get(id)
+    cat.delete()
+    #Enviar datos a form
+    return redirect(url_for('categories'))
 
 if __name__ == '__main__':
     app.run(debug=True)
